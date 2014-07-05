14764f45f86e577128b8aa93ba7f5dcaeefc98a0
R. Kyle Murphy <orclev@gmail.com>
2014-07-05 05:34:07 -0400

    Added CORS middleware

--- Main.hs
+++ Main.hs
@@ -1,6 +1,8 @@
 module Main where
 
 import qualified Network.Wai as W
+import qualified Network.Wai.Middleware.Cors as W
+import qualified Network.HTTP.Types.Method as W
 import Network.Wai.Handler.Warp (run)
 import Rest.Driver.Wai (apiToApplication)
 import Rest.Api (Router, root, route, Api, mkVersion, Some1 (Some1), (-/), (--/))
@@ -24,10 +26,28 @@ api = [(mkVersion 1 0 0, Some1 site)]
 application :: ServerData -> W.Application
 application serverData = apiToApplication (runSiteApi serverData) api
 
+corsPolicy :: W.CorsResourcePolicy
+corsPolicy = W.CorsResourcePolicy
+  { W.corsOrigins = Nothing
+  , W.corsMethods = [ W.methodGet
+                    , W.methodPost
+                    , W.methodHead
+                    , W.methodPut
+                    , W.methodDelete
+                    , W.methodOptions
+                    ]
+  , W.corsRequestHeaders = ["Content-Type"]
+  , W.corsExposedHeaders = Nothing
+  , W.corsMaxAge = Just 3600
+  , W.corsVaryOrigin = True
+  , W.corsRequireOrigin = False
+  , W.corsIgnoreFailures = False
+  }
+
 main :: IO ()
 main = do
   pool <- createSqlitePool "test.db3" 10
   flip runSqlPool pool $ do
     runResourceT $ runStderrLoggingT $ flip runSqlPool pool $ do
         runMigration migrateAll
-  run 3000 $ application (ServerData pool)
+  run 3000 $ W.cors (const $ Just corsPolicy) $ application (ServerData pool)
